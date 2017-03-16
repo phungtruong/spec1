b01c7d150662f63b93a6f6dc6780b56dcdd77ab7
phungtruong <phungtruong103@gmail.com>
2017-03-16 15:01:47 +0700

    Please Pull

--- app/src/main/java/phungtruong/com/spec1/activity/MainActivity.java
+++ app/src/main/java/phungtruong/com/spec1/activity/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Employee e1 = new Employee("e01","PhungTruong115",12);
+        Employee e2 = new Employee("e02","PhungTruong024", 13);
     }
 }
